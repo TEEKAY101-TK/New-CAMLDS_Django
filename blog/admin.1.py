c148edfada9f363bc21b1fc6e9648da932c88af8
unknown <you@example.com>
2025-08-22 15:18:25 +0100

    TEEKAY

--- blog/admin.1.py
+++ blog/admin.1.py
@@ -1,49 +1,49 @@
-from dajngo.conrtib import admin
-from django.utils import timezone
-from.models import Category, Post, Tag
+from django.contrib import admin
 
 
-class PostTagInline(admin.TabularInline):
-    model = post.tags.through
-    extra = 1
-    raw_id_fields =("tag",)
-    
-    
-@admin.register(post)
-class PostAdmin(admin.ModelAdmin)
+# Register your models here.
 
-list_display = ("title","category", "status", "published_at", "created_at", "updated")
-list_filter = ("status", "category")
-search_feilds = ("little","body")
-date_hierarchy = ("published_at",)
-ordering = ("-published_at",)
+from .models import Category, Post, Tag
 
-
-prepopulated_fields = {"slug": (title,)}
-readonly_fields = ("created_at","updated_at")
-fieldsets = (
-    ("Basic info", {"fields":("title", "slug", "status")})
-("content", {"fields":("body",)})
-("Relations & Dates", {"fields":("category", "published_at")}),
-("Timestamps", {"fields":("create", "updated_at")})
-)
-
-#Inline M2M editor: hide the default M2M widget for tags
-
-inlines = [postTagInline]
-exclude = ("tags",)
-
-#--------custom actions-------
-@admin.action(description="Mark selected posts as published")
-#Only update ones not already publushed
-count = queryset.exclude(status="published").update(
-    status="published",
-    published_at=timezone.now(),
-)
-self.message_user(request, f"{count} post(s) marked as published")
-
-actions = ["mark_as_published"]
-
-#simple registerations for category & tag
 admin.site.register(Category)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    #list view
+    list_display = ("title","category","status","published_at","created_at",)
+    list_filter = ("status","category")
+    search_fields = ("title","body")
+    date_hierarchy = "published_at"
+    ordering = ("-published_at",)
+    
+prepopulated_fields = {"slug": ("title,")}
+readonly_fields = ("created_at", "updated_at")
+fieldsets = (
+    ("Basic info", {
+        "fields": ("title", "slug", "status"),
+    }),
+    )
+("content",({
+        "fields": ("body",),
+    }),
+    )
+("Relationships & Dates", ({
+        "fields": ("category", "published_at"),
+    }),
+    )
+("Timestamps",({
+        "fields": ("created_at", "updated_at"),
+    }),
+    )
+
+
+    
 admin.site.register(Tag)
+
+
+
+
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py runserver
+# python manage.py createsuperuser
--- blog/admin.py
+++ blog/admin.py
@@ -1,49 +1,63 @@
+
+
 from django.contrib import admin
+from django.utils import timezone
+from.models import Category, Post, Tag
 
 
-# Register your models here.
-
-from .models import Category, Post, Tag
-
-admin.site.register(Category)
-
+class PostTagInline(admin.TabularInline):
+    model = Post.tags.through
+    extra = 1
+    raw_id_fields =("tag",)
+    
+    
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    #list view
-    list_display = ("title","category","status","published_at","created_at",)
-    list_filter = ("status","category")
-    search_fields = ("title","body")
-    date_hierarchy = "published_at"
+
+    list_display = ("title","category", "status", "published_at", "created_at")
+    list_filter = ("status", "category")
+    search_feilds = ("little","body")
+    date_hierarchy = ("published_at")
     ordering = ("-published_at",)
-    
-prepopulated_fields = {"slug": ("title,")}
-readonly_fields = ("created_at", "updated_at")
-fieldsets = (
-    ("Basic info", {
-        "fields": ("title", "slug", "status"),
-    }),
-    )
-("content",({
-        "fields": ("body",),
-    }),
-    )
-("Relationships & Dates", ({
-        "fields": ("category", "published_at"),
-    }),
-    )
-("Timestamps",({
-        "fields": ("created_at", "updated_at"),
-    }),
-    )
 
 
-    
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("created_at","updated_at")
+    fieldsets = (
+    ("Basic info", {"fields": ("title", "slug", "status")}),
+    ("content", {"fields":("body",)}),
+    ("Relations & Dates", {"fields":("category", "published_at")}),
+    ("Timestamps", {"fields":("created_at", "updated_at")}),
+    )
+
+    #Inline M2M editor: hide the default M2M widget for tags
+
+    inlines = [PostTagInline]
+    exclude = ("tags",)
+
+    #--------custom actions-------
+    @admin.action(description="Mark selected posts as published")
+    def mark_as_published(self,request,queryset):
+        #Only update ones not already publushed
+        count = queryset.exclude(status="published").update(
+        status="published",
+         published_at=timezone.now(),
+     )
+        self.message_user(request, f"{count} post(s) marked as published")
+
+    actions = ["mark_as_Draft"]
+
+    @admin.action(description="Mark selected posts as Draft")
+    def mark_as_published(self,request,queryset):
+        #Only update ones not already publushed
+        count = queryset.exclude(status="draft").update(
+        status="draft",
+         published_at=timezone.now(),
+        )
+        self.message_user(request, f"{count} post(s) marked as published")
+
+    actions = ["mark_as_Draft"]
+
+#simple registerations for category & tag
+admin.site.register(Category)
 admin.site.register(Tag)
-
-
-
-
-# python manage.py makemigrations
-# python manage.py migrate
-# python manage.py runserver
-# python manage.py createsuperuser
